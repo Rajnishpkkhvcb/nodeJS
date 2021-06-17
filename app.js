fb477806fc6fb71a39d4fb81c256c49816e7c028
Rajnishpkkhvcb <60421387+Rajnishpkkhvcb@users.noreply.github.com>
2021-06-17 22:02:48 +0530

    middlewares added

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const http= require('http');
-const routes=require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const Server =http.createServer(routes.handler);
+const app = express();
 
-console.log(routes.sometext)
+app.use(bodyParser.urlencoded({extended: false}));
 
-Server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
 
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add No of Products</button></form>');
+  });
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
