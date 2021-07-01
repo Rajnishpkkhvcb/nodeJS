6e0a2581911b19ce518bac1b799dc1e8bd9f6965
Rajnishpkkhvcb <60421387+Rajnishpkkhvcb@users.noreply.github.com>
2021-07-01 22:05:29 +0530

    cart model created

--- shop.js
+++ shop.js
@@ -1,21 +1,62 @@
-const path = require('path');
+const Product = require('../models/product');
+const Cart = require('../models/cart');
 
-const express = require('express');
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
 
-const shopController = require('../controllers/shop');
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
+};
 
-const router = express.Router();
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
 
-router.get('/', shopController.getIndex);
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
 
-router.get('/products', shopController.getProducts);
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
 
-router.get('/products/:productId', shopController.getProduct);
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
 
-router.get('/cart', shopController.getCart);
-
-router.get('/orders', shopController.getOrders);
-
-router.get('/checkout', shopController.getCheckout);
-
-module.exports = router;
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
