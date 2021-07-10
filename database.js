87440d48980823abc6366dc623ef7e82d0498b33
Rajnishpkkhvcb <60421387+Rajnishpkkhvcb@users.noreply.github.com>
2021-07-10 17:38:18 +0530

    sequelize

--- database.js
+++ database.js
@@ -1,11 +1,12 @@
 const mysql= require('mysql2');
 
+const Sequelize= require('sequelize');
 
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'raj',
-    password:'priraj1336'
-})
+const sequelize=new Sequelize('raj','root','priraj1336',{
+    dialect:'mysql',
+    host:'localhost'
+});
 
-module.exports=pool.promise();
+
+module.exports=sequelize;
+
